4a781f0100e3f824abdca5cad0e03bad1a478ea2
Yuliia Korabelska <yuliia_korabelska@epam.com>
2020-05-06 19:33:53 +0300

    add user endpoint for users

--- src/main/java/ua/nure/korabelska/agrolab/controller/UserController.java
+++ src/main/java/ua/nure/korabelska/agrolab/controller/UserController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import ua.nure.korabelska.agrolab.dto.AdminUserDto;
 import ua.nure.korabelska.agrolab.dto.RegistrationUserDto;
 import ua.nure.korabelska.agrolab.dto.UserDto;
 import ua.nure.korabelska.agrolab.exception.UserNotFoundException;
@@ -12,6 +13,8 @@ import ua.nure.korabelska.agrolab.security.jwt.JwtTokenProvider;
 import ua.nure.korabelska.agrolab.service.UserService;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "api/v1/users")
@@ -63,6 +66,26 @@ public class UserController {
         UserDto result = UserDto.fromUser(editedUser);
 
         return new ResponseEntity<>(result, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<AdminUserDto> getUserById(@PathVariable(name = "id") Long id) {
+        User user = userService.findById(id);
+
+        if (user == null) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+
+        AdminUserDto result = AdminUserDto.fromUser(user);
+
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<AdminUserDto>> getAllUsers() {
+        List<User> users = userService.getAll();
+        List<AdminUserDto> userDtos = users.stream().map(AdminUserDto::fromUser).collect(Collectors.toList());
+        return ResponseEntity.ok().body(userDtos);
     }
 
 }
