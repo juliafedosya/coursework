834cb84b6740cbb1cd07f0de43e38ea76157f337
Yuliia Korabelska <yuliia_korabelska@epam.com>
2020-06-11 21:14:39 +0300

    attempt to solve deploy issues

--- src/main/java/ua/nure/korabelska/agrolab/service/impl/UserServiceImpl.java
+++ src/main/java/ua/nure/korabelska/agrolab/service/impl/UserServiceImpl.java
@@ -2,6 +2,7 @@ package ua.nure.korabelska.agrolab.service.impl;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import ua.nure.korabelska.agrolab.dto.RegistrationUserDto;
@@ -23,6 +24,7 @@ public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
     private final RoleRepository roleRepository;
+    @Lazy
     private final BCryptPasswordEncoder passwordEncoder;
 
     @Autowired
